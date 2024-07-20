dd58c28089315602131ccee8ed1022c626adb35c
Hamza <153912992+MuhammadHamzaAslam@users.noreply.github.com>
2024-07-20 17:02:01 +0500

    working

---
+++ Files/app.js
@@ -0,0 +1,32 @@
+const dynamicText = document.querySelector("h1 span");
+const words = ["Generate Box Shadows, Borders, and QR Codes Effortlessly", "Craft Beautiful Shadows and Borders Instantly", "Transform Your Designs with Our Interactive Tools", "Create Stunning Visual Effects with Ease"];
+
+// Variables to track the position and deletion status of the word
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+const typeEffect = () => {
+    const currentWord = words[wordIndex];
+    const currentChar = currentWord.substring(0, charIndex);
+    dynamicText.textContent = currentChar;
+    dynamicText.classList.add("stop-blinking");
+
+    if (!isDeleting && charIndex < currentWord.length) {
+        // If condition is true, type the next character
+        charIndex++;
+        setTimeout(typeEffect, 100);
+    } else if (isDeleting && charIndex > 0) {
+        // If condition is true, remove the previous character
+        charIndex--;
+        setTimeout(typeEffect, 100);
+    } else {
+        // If word is deleted then switch to the next word
+        isDeleting = !isDeleting;
+        dynamicText.classList.remove("stop-blinking");
+        wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
+        setTimeout(typeEffect, 200);
+    }
+}
+
+typeEffect();
